df708d73caf920a0e47782cc3ef7d0690c79d8ef
Ray O <71047193+cs-cmd@users.noreply.github.com>
2024-01-12 16:06:42 +0000

    Moved static assets (images, fonts, icons) to 'public' folder; Implemented PostCSS (PostCSS Nesting) for CSS nesting support in CodeSandbox enviornment;

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,48 +1,54 @@
-import ImageAndDesc from '../../ImageAndDesc/ImageAndDesc';
-import './HomePage.css';
+import ImageAndDesc from "../../ImageAndDesc/ImageAndDesc";
+import "./HomePage.css";
 
 const homePageImageContent = [
   {
-    imgSrc: '/images/factory-images/factory2.jpg',
-    desc: 'Late-stage production of warehouse inventory',
-    text: 'Made from exotically-sourced nutrients from around the world, CityCube strives to nourish the City and neightboring boroughs.'
-  }, 
-  {
-    imgSrc: '/images/founders/founders1.jpeg',
-    desc: 'The founders, Raz Keenz and Mohammad Kapoor, in-front of their first factory',
-    text: 'Designed from the ground up by CS Financial\'s own son, Raz Keenz, alongside friend Mohammad Kapoor, built a culinary empire spanning the City.'
+    imgSrc: new URL("/images/factory-images/factory2.jpg", import.meta.url)
+      .href,
+    desc: "Late-stage production of warehouse inventory",
+    text: "Made from exotically-sourced nutrients from around the world, CityCube strives to nourish the City and neightboring boroughs.",
   },
   {
-    imgSrc: '/images/food-images/food1.jpg',
-    desc: 'A staple dish, the Orange Meringue Block',
-    text: 'Made efficiently from natural and artificial flavorings and nutrients sourced from around the world, CityCube\'s unit of food, the Block, is calorie-dense and stable, capable of filling a grown man in just two servings!'
-  }
-]
+    imgSrc: new URL("/images/founders/founders1.jpeg", import.meta.url).href,
+    desc: "The founders, Raz Keenz and Mohammad Kapoor, in-front of their first factory",
+    text: "Designed from the ground up by CS Financial's own son, Raz Keenz, alongside friend Mohammad Kapoor, built a culinary empire spanning the City.",
+  },
+  {
+    imgSrc: new URL("/images/food-images/food1.jpg", import.meta.url).href,
+    desc: "A staple dish, the Orange Meringue Block",
+    text: "Made efficiently from natural and artificial flavorings and nutrients sourced from around the world, CityCube's unit of food, the Block, is calorie-dense and stable, capable of filling a grown man in just two servings!",
+  },
+];
 
 export default function HomePage() {
   return (
     <main className="home-page">
-      <section className='image-section'>
-        <div className='blur-container'>
-          <h1 className='heading'>Welcome to CityCube!</h1>
+      <section className="image-section">
+        <div className="blur-container">
+          <h1 className="heading">Welcome to CityCube!</h1>
           <p>A quick-eats provider for the People of the City.</p>
           <p>A staple of City cuisine and culture.</p>
           <p>A paragon of high-quality food and speedy delivery.</p>
-            
+
           <h1>Never worry about the complexity of food again!</h1>
-          
-          <a href='/menu' className='page-button-link hidden-link'>Order now!</a>
-          <div className='transparant-fade'/>
+
+          <a href="/menu" className="page-button-link hidden-link">
+            Order now!
+          </a>
+          <div className="transparant-fade" />
         </div>
       </section>
 
-      <section className='home-page-image-display'> 
-        {homePageImageContent.map((item, index) => 
-          <ImageAndDesc imgSrc={item.imgSrc} 
-            key={index} 
+      <section className="home-page-image-display">
+        {homePageImageContent.map((item, index) => (
+          <ImageAndDesc
+            imgSrc={item.imgSrc}
+            key={index}
             desc={item.desc}
-            text={item.text}/>)}
+            text={item.text}
+          />
+        ))}
       </section>
     </main>
-  )
+  );
 }
