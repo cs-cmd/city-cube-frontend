441aeb9d5e3cec71f8e180aef45498e45e5445bf
Ray <rayoneill.97@gmail.com>
2024-02-02 14:33:43 -0500

    Added fetch to get data from server;

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -4,29 +4,44 @@ import SectionBreak from "@components/SectionBreak/SectionBreak";
 import "./MenuPage.css";
 import { useState } from "react";
 
+const LoadTimeline = {
+  LOADING: 1,
+  FOUND: 2,
+  ERROR: 3,
+}
 export default function MenuPage() {
   const [menuItems, setMenuItems] = useState(null);
+  const [timeline, setTimeline] = useState(LoadTimeline.LOADING);
 
   // const menuItems = await menuObjectsts;
   // add filter option for flavors, price, etc.
-  const menuQueryResults = menuObjects;
-  const newMenuItems = menuQueryResults.map((menuObject, index) => (
-    <MenuItem key={index} {...menuObject} />
-  ));
-  if (menuItems === null) {
-    setMenuItems(newMenuItems);
-  }
-
+  // const menuQueryResults = menuObjects;
+  fetch('http://localhost:3000/api/menu-items')
+    .then(res => res.json())
+    .then(json => { 
+      console.log(json);
+      setMenuItems(json.map((menuObject, index) => {
+        return (<MenuItem key={index} {...menuObject}/>);
+      }));
+      setTimeline(LoadTimeline.FOUND)
+    })
+    .catch(err => {
+      console.log(err);
+      setTimeline(LoadTimeline.ERROR);
+    });
+  
   return (
     <main className="menu-page">
       <h1 className="page-header">Our Menu</h1>
       <SectionBreak />
       <section className="menu-section">
-        {menuItems === null ? (
-          <p className="centered-text">Loading Menu...</p>
-        ) : (
-          menuItems
-        )}
+       { 
+       timeline === LoadTimeline.LOADING && <p className='centered-text'>Loading menu...</p> 
+        ||
+        timeline === LoadTimeline.FOUND && menuItems
+        ||
+        timeline === LoadTimeline.ERROR && <p className='centered-text'>Can&apos;t get menu at the moment; please check again later!</p>
+       }
       </section>
     </main>
   );
